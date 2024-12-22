897a8030be4e25f20c2a7740dda277a13fe7e7a1
Mo2 . <moses.uriarte@lc.cuny.edu>
2024-12-21 22:02:18 -0500

    Added messages model and controller, updated index.js for messages route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import {connectDB} from './lib/db.js'
 import authRoutes from "./routes/auth.route.js"
+import message from "./models/message.route.js";
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT:' + PORT)
